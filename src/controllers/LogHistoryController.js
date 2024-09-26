cea69038c5ce1062c50045934624eaf42da4c7e0
Luiz Gustavo <luizgustavobonke@gmail.com>
2024-09-26 16:17:53 -0300

    Controller e Model da Janela de Registro de Acesso + Util

---
+++ src/controllers/LogHistoryController.js
@@ -0,0 +1,27 @@
+
+const { dialog } = require('electron');
+const LogHistoryModel = require('../models/LogHistoryModel.js');
+const windowManager = require('../../windows.js');
+
+async function LogHistoryController(id) {
+  try {
+    if (!id) return false;
+
+    const LogUser = new LogHistoryModel(id);
+    await LogUser.initLog();
+
+    let string = 'Confira os campos abaixo:\n\n';
+    LogUser.errors.forEach(val => string += val);
+
+    if (LogUser.errors.length > 0) {
+      dialog.showErrorBox('Não foi possível acessar o histórico', `\n${string}`);
+      return false;
+    }
+
+    return LogUser.logUser;
+  } catch (e) {
+    console.error('Erro ao tentar localizar o histórico de acesso do registro:', e);
+  }
+}
+
+module.exports = LogHistoryController;
---
+++ src/models/LogHistoryModel.js
@@ -0,0 +1,48 @@
+const { FindOne } = require('../util/dbRepository.js');
+const getLogAccessByID = require('../util/getLogAccessByID.js');
+
+let sql = null;
+
+class LogHistoryModel {
+  constructor(id) {
+    this.id = id;
+    this.logUser = null;
+    this.errors = [];
+  }
+
+  async initLog() {
+    await this.validate();
+  
+    this.logUser = await getLogAccessByID(this.id);
+  }
+
+  async validate() {
+    if (!await this.idExists()) this.errors.push('Registro não localizado');
+    if (!await this.userExistsLogHistory()) 
+    this.errors.push('A pessoa selecionada não possui registros de acesso ao prédio');
+  }
+
+  async idExists() {
+    try {
+      sql = 'SELECT cad_id FROM Cadastro WHERE cad_id = (?)';
+      const row = await FindOne(sql, [this.id]);
+      return row;
+    } catch (e) {
+      console.error('Erro ao tentar localizar o registro:', e);
+    }
+  }
+
+  async userExistsLogHistory() {
+    try {
+      sql = 'SELECT cad_id FROM Acesso_Historico WHERE cad_id = (?)';
+      const row = await FindOne(sql, [this.id]);
+      return row;
+    } catch (e) {
+      console.error('Erro ao tentar verificar a existência de acesso ao prédio: ', e);
+    }
+  }
+
+}
+
+module.exports = LogHistoryModel;
+
